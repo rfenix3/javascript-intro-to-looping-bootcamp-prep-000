ba39a192b92cae653cec20a13b2a5ab7ded56dd3
Richard Fenix <richard.fenix@gmail.com>
2018-02-23 07:37:23 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -13,8 +13,12 @@ function whileLoop(number){
    return 'done';
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
   do {
-    
-  } while (maybeTrue());
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
 }
